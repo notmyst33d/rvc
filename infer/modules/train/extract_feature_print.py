148deb06d0069c6b856a9da56e180b6092ba44a7
Myst33d <notmyst33d@gmail.com>
2023-12-29 13:42:32 +0600

    Sync

--- infer/modules/train/extract_feature_print.py
+++ infer/modules/train/extract_feature_print.py
@@ -80,7 +80,7 @@ printt("load model(s) from {}".format(model_path))
 # if hubert model is exist
 if os.access(model_path, os.F_OK) == False:
     printt(
-        "Error: Extracting is shut down because %s does not exist."
+        "Error: Extracting is shut down because %s does not exist, you may download it from https://huggingface.co/lj1995/VoiceConversionWebUI/tree/main"
         % model_path
     )
     exit(0)
